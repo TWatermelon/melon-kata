58e684fc40729256297226a4a92e726e805baeea
Waterstrong <sqlin@thoughtworks.com>
2016-07-02 14:08:09 +0800

    [M003][Waterstrong] - inline lamda

--- src/main/java/tw/kata/lijun/CollectionUtil.java
+++ src/main/java/tw/kata/lijun/CollectionUtil.java
@@ -2,7 +2,6 @@ package tw.kata.lijun;
 
 import static java.util.stream.Collectors.toList;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionUtil {
@@ -11,12 +10,6 @@ public class CollectionUtil {
     }
 
     public static List<Integer> getEvens(List<Integer> numberList) {
-        List<Integer> evenList = new ArrayList<Integer>();
-        for (Integer i : numberList) {
-            if (i % 2 == 0) {
-                evenList.add(i);
-            }
-        }
-        return evenList;
+        return numberList.stream().filter(i -> i % 2 == 0).collect(toList());
     }
 }
