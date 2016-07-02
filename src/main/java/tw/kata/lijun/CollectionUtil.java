65b339d753b897c89c87b6fca9184f22c02e553f
lijun003 <lijun359714823@163.com>
2016-07-02 13:16:53 +0800

    [M003][Lijun] - implement get odds method

--- src/main/java/tw/kata/lijun/CollectionUtil.java
+++ src/main/java/tw/kata/lijun/CollectionUtil.java
@@ -2,8 +2,10 @@ package tw.kata.lijun;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class CollectionUtil {
     public static List<Integer> getOdds(List<Integer> numberList) {
-        return null;
+        return numberList.stream().filter(num -> num % 2 != 0).collect(toList());
     }
 }
--- src/test/java/tw/kata/lijun/CollectionUtilTest.java
+++ src/test/java/tw/kata/lijun/CollectionUtilTest.java
@@ -1,22 +1,19 @@
 package tw.kata.lijun;
 
-import static java.util.Arrays.asList;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import org.junit.Test;
 
 import java.util.List;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static tw.kata.lijun.CollectionUtil.getOdds;
 
 public class CollectionUtilTest {
-    @Ignore
     @Test
     public void should_get_odd_numbers_given_number_list() {
         List<Integer> numberList = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        List<Integer> odds = CollectionUtil.getOdds(numberList);
-
-        assertThat(odds, is(asList(1, 3, 5, 7, 9)));
+        assertThat(getOdds(numberList), is(asList(1, 3, 5, 7, 9)));
     }
 }
