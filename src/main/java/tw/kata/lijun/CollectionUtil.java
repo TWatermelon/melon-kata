4b8ba3f091de8f34cbb8391c4560b01b5a8161cb
lijun003 <lijun359714823@163.com>
2016-07-02 13:29:32 +0800

    [M003][Lijun] - add get evens test

--- src/main/java/tw/kata/lijun/CollectionUtil.java
+++ src/main/java/tw/kata/lijun/CollectionUtil.java
@@ -8,4 +8,8 @@ public class CollectionUtil {
     public static List<Integer> getOdds(List<Integer> numberList) {
         return numberList.stream().filter(num -> num % 2 != 0).collect(toList());
     }
+
+    public static List<Integer> getEvens(List<Integer> numberList) {
+        return null;
+    }
 }
--- src/test/java/tw/kata/lijun/CollectionUtilTest.java
+++ src/test/java/tw/kata/lijun/CollectionUtilTest.java
@@ -1,8 +1,10 @@
 package tw.kata.lijun;
 
-import org.junit.Test;
-
 import java.util.List;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
@@ -10,10 +12,22 @@ import static org.junit.Assert.assertThat;
 import static tw.kata.lijun.CollectionUtil.getOdds;
 
 public class CollectionUtilTest {
+
+    private List<Integer> numberList;
+
+    @Before
+    public void setUp() throws Exception {
+        numberList = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
     @Test
     public void should_get_odd_numbers_given_number_list() {
-        List<Integer> numberList = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
         assertThat(getOdds(numberList), is(asList(1, 3, 5, 7, 9)));
+    }
+
+    @Ignore
+    @Test
+    public  void should_get_even_numbers_given_number_list() {
+        assertThat(CollectionUtil.getEvens(numberList), is(asList(2, 4, 6, 8,10)));
     }
 }
